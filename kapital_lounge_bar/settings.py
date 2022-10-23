8a2a9bcd53b662818189984d3ee23c777af4ddff
Gustavo Honorio <gustavo.honorio@yahoo.com.br>
2022-10-23 13:36:15 -0300

    Adicionando user-agents para adaptar o navbar de acordo com o dispositivo do usuario

--- kapital_lounge_bar/settings.py
+++ kapital_lounge_bar/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # others apps
+    'django_user_agents',
+
     # apps
     'core',
     'gestao',
@@ -50,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_user_agents.middleware.UserAgentMiddleware',
     # descomentar quando subir para producao
     # 'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
@@ -129,3 +133,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+USER_AGENTS_CACHE = 'default'
